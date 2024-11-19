99091f0598fc35b8fdc9b9eade3c3791aaa865a4
david <alireza.d159@outlook.com>
2024-11-19 08:49:25 +0330

    [add] authentication and authurisation using jwt

--- backend/apis/base.py
+++ backend/apis/base.py
@@ -1,3 +1,4 @@
+from apis.v1 import route_login
 from apis.v1 import route_order
 from apis.v1 import route_user
 from fastapi import APIRouter
@@ -6,3 +7,4 @@ from fastapi import APIRouter
 api_router = APIRouter()
 api_router.include_router(route_user.router, prefix="", tags=["users"])
 api_router.include_router(route_order.router, prefix="", tags=["orders"])
+api_router.include_router(route_login.router, prefix="", tags=["blogs"])
