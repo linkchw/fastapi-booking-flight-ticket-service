2b65a1e2f2fe9a04c4dc7f6f202fa9f83dc7f125
david <alireza.d159@outlook.com>
2024-11-23 03:05:21 +0330

    [pre]

--- backend/apis/v1/route_order.py
+++ backend/apis/v1/route_order.py
@@ -1,18 +1,17 @@
-from fastapi import Depends
-from fastapi import status
-from fastapi import HTTPException
-from schemas.order import OrderCreate
-from schemas.order import Order
-from sqlalchemy.orm import Session
-from sqlalchemy.exc import IntegrityError
+from db.models.passenger import Passenger
+from db.models.ticket import Ticket
+from db.models.user import User
 from db.repository.order import create_new_order
 from db.repository.passenger import create_new_passenger
 from db.session import get_db
-from db.models.user import User
-from db.models.passenger import Passenger
-from db.models.ticket  import Ticket
-
 from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import HTTPException
+from fastapi import status
+from schemas.order import Order
+from schemas.order import OrderCreate
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session
 
 
 router = APIRouter()
@@ -23,14 +22,20 @@ async def submit_order(order: OrderCreate, db: Session = Depends(get_db)):
     try:
         user = db.query(User).filter(User.id == order.user_id).first()
         if not user:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User not found")
-        
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail="User not found"
+            )
+
         new_order = create_new_order(order=order, db=db)
 
         for ticket_data in order.tickets:
             passenger_data = ticket_data.passenger
 
-            passenger = db.query(Passenger).filter(Passenger.national_id == passenger_data.national_id).first()
+            passenger = (
+                db.query(Passenger)
+                .filter(Passenger.national_id == passenger_data.national_id)
+                .first()
+            )
 
             if not passenger:
                 passenger = Passenger(
@@ -46,7 +51,7 @@ async def submit_order(order: OrderCreate, db: Session = Depends(get_db)):
             ticket = Ticket(
                 ticket_number=ticket_data.ticket_number,
                 passenger_id=passenger.id,
-                order_id=new_order.id
+                order_id=new_order.id,
             )
             db.add(ticket)
 
@@ -56,5 +61,7 @@ async def submit_order(order: OrderCreate, db: Session = Depends(get_db)):
 
     except IntegrityError as e:
         db.rollback()
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Order code or ticket number must be unique")
-
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Order code or ticket number must be unique",
+        )
