50a7cc25689d62f8e12cfbb5b169453093a34b22
david <alireza.d159@outlook.com>
2024-11-22 11:38:41 +0330

    [start] order submit route

--- backend/apis/base.py
+++ backend/apis/base.py
@@ -1,10 +1,16 @@
-# from apis.v1 import route_login
+from apis.v1 import route_ticket
 from apis.v1 import route_order
+
+# from apis.v1 import route_login
 # from apis.v1 import route_user
+
 from fastapi import APIRouter
 
-
 api_router = APIRouter()
+
+
+api_router.include_router(route_ticket.router, prefix="", tags=["tickets"])
+api_router.include_router(route_order.router, prefix="", tags=["blogs"])
+
 # api_router.include_router(route_user.router, prefix="", tags=["users"])
-api_router.include_router(route_order.router, prefix="", tags=["orders"])
 # api_router.include_router(route_login.router, prefix="", tags=["blogs"])
